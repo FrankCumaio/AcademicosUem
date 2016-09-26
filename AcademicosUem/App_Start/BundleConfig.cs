16a37d963102fd7bfedcf74f12bcde9379b582db
Mr. Belton <inercio28@gmail.com>
2016-09-26 15:26:03 +0200

    Actualizacao do bundle config

--- AcademicosUem/App_Start/BundleConfig.cs
+++ AcademicosUem/App_Start/BundleConfig.cs
@@ -16,16 +16,39 @@ namespace AcademicosUem
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
 
+            //add no template
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*",
+                        "~/Scripts/init.js",
+                        "~/Scripts/jquery-2.1.1.min.js",
+                        "~/Scripts/materialize.js",
+                        "~/Scripts/materialize.min.js",
+                        "~/Scripts/modernizr.js"));
+            //add no template
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            //add no template
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/font-awesome.min.css",
+                      "~/Content/materialize.css",
+                      "~/Content/materialize.min.css",
+                      "~/Content/style.css"));
+
+            //add no template
+            bundles.Add(new ScriptBundle("~/bundles/minjs").Include(
+                     "~/Scripts/custom-min.js",
+                     "~/Scripts/plugin-min.js"));
+
+            //add no template
+            bundles.Add(new StyleBundle("~/bundles/mincss").Include(
+                     "~/Content/custom-min.css",
+                     "~/Content/plugin-min.css"));
+
         }
     }
 }
