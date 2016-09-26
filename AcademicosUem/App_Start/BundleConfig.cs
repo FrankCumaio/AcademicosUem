65bafb8f3b00206f3f84faafd8bd4320b317eb05
Mr. Belton <inercio28@gmail.com>
2016-09-26 22:39:22 +0200

    Adicao do template

--- AcademicosUem/App_Start/BundleConfig.cs
+++ AcademicosUem/App_Start/BundleConfig.cs
@@ -25,6 +25,17 @@ namespace AcademicosUem
                         "~/Scripts/materialize.js",
                         "~/Scripts/materialize.min.js",
                         "~/Scripts/modernizr.js"));
+
+            bundles.Add(new ScriptBundle("~/Scripts/js").Include(
+                      "~/Scripts/init.js",
+                      "~/Scripts/jquery-2.1.1.min.js",
+                      "~/Scripts/materialize.js",
+                      "~/Scripts/materialize.min.js",
+                      "~/Scripts/modernizr.js",
+                      "~/Scripts/custom-min.js",
+                     "~/Scripts/plugin-min.js"));
+
+
             //add no template
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
@@ -33,21 +44,13 @@ namespace AcademicosUem
             //add no template
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css",
+                      "~/Content/Site.css",
                       "~/Content/font-awesome.min.css",
                       "~/Content/materialize.css",
                       "~/Content/materialize.min.css",
-                      "~/Content/style.css"));
-
-            //add no template
-            bundles.Add(new ScriptBundle("~/bundles/minjs").Include(
-                     "~/Scripts/custom-min.js",
-                     "~/Scripts/plugin-min.js"));
-
-            //add no template
-            bundles.Add(new StyleBundle("~/bundles/mincss").Include(
-                     "~/Content/custom-min.css",
-                     "~/Content/plugin-min.css"));
+                      "~/Content/style.css",
+                      "~/Content/custom-min.css",
+                      "~/Content/plugin-min.css"));
 
         }
     }
--- AcademicosUem/Scripts/init.js
+++ AcademicosUem/Scripts/init.js
@@ -12,9 +12,9 @@
 		barAnimationDelay = 3800,
 		barWaiting = barAnimationDelay - 3000, //3000 is the duration of the transition on the loading bar - set in the scss/css file
 		//letters effect
-		lettersDelay = 50,
+		lettersDelay = 40,
 		//type effect
-		typeLettersDelay = 150,
+		typeLettersDelay = 120,
 		selectionDuration = 500,
 		typeAnimationDelay = selectionDuration + 800,
 		//clip effect 
