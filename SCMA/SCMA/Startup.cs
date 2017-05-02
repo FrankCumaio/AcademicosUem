11629361329a66c19f794fb12e5dd80797436420
Frank Cumaio <Frank Cumaio@DESKTOP-CKV44ER>
2017-05-02 16:07:56 +0200

    1st commit

---
+++ SCMA/SCMA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SCMA.Startup))]
+namespace SCMA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
